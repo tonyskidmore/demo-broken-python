aff22bf21b47b6ae46485609154993836eeb4727
Tony Skidmore <anthony.skidmore@accenture.com>
2017-11-22 19:18:47 +0000

    adding a loop

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 This is a demo project for a demonstration of using git.  
 
+You need to fix it!
+
--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@
 
 from __future__ import print_function
 
+for x in range(0,5):
 print("Hello Git User!")
 
